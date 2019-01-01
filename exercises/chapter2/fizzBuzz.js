1b2e7fb20c98c6c1a39ceac0d49a62d4e6f702ac
petercolella <peter.joseph.colella@gmail.com>
2018-12-31 23:50:22 -0500

    added official FizzBuzz solution

--- exercises/chapter2/fizzBuzz.js
+++ exercises/chapter2/fizzBuzz.js
@@ -9,3 +9,10 @@ for (let i = 0; i < 100; i++) {
     console.log(i);
   }
 }
+
+for (let n = 1; n <= 100; n++) {
+  let output = '';
+  if (n % 3 == 0) output += 'Fizz';
+  if (n % 5 == 0) output += 'Buzz';
+  console.log(output || n);
+}
